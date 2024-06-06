4345bdcc2a85a630462ff122253cb63893a5ba32
vuppann25 <amazing.annika.vuppala@gmail.com>
2024-06-05 19:49:27 -0700

    health collectibles, saws, traps

---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+   [Header ("Health")]
+   [SerializeField] private float startingHealth;
+   public float currentHealth { get; private set; }      //changed to public so the healthbar script can access, added some constructer things to make sure cant be accessed from anywhere
+   private Animator anim;
+   private bool dead;
+
+   [Header ("iFrames")]
+   [SerializeField] private float invulnerabilityDuration;
+
+   private void Awake() {
+       currentHealth = startingHealth;
+       anim = GetComponent<Animator>();
+   }
+
+   public void TakeDamage(float _damage){
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0) {
+            //checking if player hurt
+            anim.SetTrigger("hurt");
+            //iframs
+        }
+        else {
+            if (!dead){
+            //player dead
+                anim.SetTrigger("die");
+                GetComponent<PlayerMovement>().enabled = false;
+                dead = true;
+            }
+        }
+   }
+
+   public void AddHealth(float _value){
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+   }
+
+   /*private void Update() {
+        if (Input.GetKeyDown(KeyCode.E)){
+            TakeDamage(1);
+        }
+   }*/
+}
