e01a90f95801ce14c75193fcdd8032833decd3e4
vuppann25 <amazing.annika.vuppala@gmail.com>
2024-06-06 00:31:58 -0700

    camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private float speed;
+    private float currentPosX;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Update() {
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed*Time.deltaTime);
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void MoveToNewRoom(Transform _newRoom){
+        currentPosX = _newRoom.position.x;
     }
 }
